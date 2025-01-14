b139457c6631ebff53315d52522dbeebb80da7cf
Roy P A <roy@nintrivalabs.com>
2025-01-14 11:38:33 +0530

    fix: splash screen created

--- lib/core/configs/constants/string_constants.dart
+++ lib/core/configs/constants/string_constants.dart
@@ -1,5 +1,5 @@
 class Strings {
-  static const String flashLight = "FLASHLIGHT";
+  static const String bmiCalculator = "BMI CALCULATOR";
   static const String flashLight1 = "FLASHLIGHT : ";
   static const String on = "ON";
   static const String off= "OFF";
